56f67029fcad7f0fb7bc7c054e040412d7ac940c
Hafiz Badrie Lubis <hafiz@jakpost.com>
2013-03-05 11:15:59 +0700

    revision at todolist_controller

--- public/js/app/controller.js
+++ public/js/app/controller.js
@@ -5,23 +5,23 @@ function todolist_controller($scope) {
 		{text: "This is the third todo with angular", done:false},
 	];
 
-	$scope.not_finish = true;
-
 	$scope.add_todo = function() {
 		$scope.todos.push({text:$scope.todo_text, done:false});
 		$scope.todo_text = '';
 	}
 
 	$scope.show_close_button = function() {
-		angular.forEach($scope.todos, function(todo) {
-			if (todo.done) {
-				$scope.not_finish = false;
-			} else {
-				$scope.not_finish = true;
+		var found = false;
+		var idx = 0;
+		while (!found && idx < $scope.todos.length) {
+			if (!$scope.todos[idx].done) {
+				found = true;
 			}
-		});
 
-		if (!$scope.not_finish) {
+			idx++;
+		}
+
+		if (!found) {
 			angular.element("#btn-close").removeAttr('disabled');
 		} else {
 			angular.element("#btn-close").attr('disabled', '');
