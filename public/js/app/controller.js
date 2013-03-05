f9d340ee0d0dad132105c232b6a5630e622db316
Hafiz Badrie Lubis <hafiz@jakpost.com>
2013-03-05 11:09:56 +0700

    add todolist page

--- public/js/app/controller.js
+++ public/js/app/controller.js
@@ -1,8 +1,30 @@
 function todolist_controller($scope) {
-	$scope.todos = [];
+	$scope.todos = [
+		{text: "This is the first todo with angular", done:true},
+		{text: "This is the second todo with angular", done:false},
+		{text: "This is the third todo with angular", done:false},
+	];
+
+	$scope.not_finish = true;
 
 	$scope.add_todo = function() {
 		$scope.todos.push({text:$scope.todo_text, done:false});
 		$scope.todo_text = '';
+	}
+
+	$scope.show_close_button = function() {
+		angular.forEach($scope.todos, function(todo) {
+			if (todo.done) {
+				$scope.not_finish = false;
+			} else {
+				$scope.not_finish = true;
+			}
+		});
+
+		if (!$scope.not_finish) {
+			angular.element("#btn-close").removeAttr('disabled');
+		} else {
+			angular.element("#btn-close").attr('disabled', '');
+		}
 	}
 }
